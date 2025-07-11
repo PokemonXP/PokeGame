8b545e68f7367f87e6a6c9706f79414bccb8335f
Francis Pion <francispion@hotmail.com>
2025-07-10 21:41:35 -0400

    Refactored BaseStatistics.

--- frontend/src/components/pokemon/BaseStatistics.vue
+++ frontend/src/components/pokemon/BaseStatistics.vue
@@ -4,72 +4,38 @@ import { useI18n } from "vue-i18n";
 
 import type { BaseStatistics } from "@/types/pokemon";
 
+const items: (keyof BaseStatistics)[] = ["hp", "attack", "defense", "specialAttack", "specialDefense", "speed"];
 const { t } = useI18n();
 
 defineProps<{
   statistics: BaseStatistics;
 }>();
+
+function formatForId(statistic: keyof BaseStatistics): string {
+  switch (statistic) {
+    case "specialAttack":
+      return "special-attack";
+    case "specialDefense":
+      return "special-defense";
+  }
+  return statistic;
+}
+function formatForLabel(statistic: keyof BaseStatistics): string {
+  return statistic === "hp" ? statistic.toUpperCase() : statistic[0].toUpperCase() + statistic.substring(1);
+}
 </script>
 
 <template>
   <div class="row mb-3">
     <TarInput
+      v-for="statistic in items"
+      :key="statistic"
       class="col"
       disabled
       floating
-      id="hp-base"
-      :label="t('pokemon.statistic.select.options.HP')"
-      :model-value="statistics.hp"
-      step="1"
-      type="number"
-    />
-    <TarInput
-      class="col"
-      disabled
-      floating
-      id="attack-base"
-      :label="t('pokemon.statistic.select.options.Attack')"
-      :model-value="statistics.attack"
-      step="1"
-      type="number"
-    />
-    <TarInput
-      class="col"
-      disabled
-      floating
-      id="defense-base"
-      :label="t('pokemon.statistic.select.options.Defense')"
-      :model-value="statistics.defense"
-      step="1"
-      type="number"
-    />
-    <TarInput
-      class="col"
-      disabled
-      floating
-      id="special-attack-base"
-      :label="t('pokemon.statistic.select.options.SpecialAttack')"
-      :model-value="statistics.specialAttack"
-      step="1"
-      type="number"
-    />
-    <TarInput
-      class="col"
-      disabled
-      floating
-      id="special-defense-base"
-      :label="t('pokemon.statistic.select.options.SpecialDefense')"
-      :model-value="statistics.specialDefense"
-      step="1"
-      type="number"
-    />
-    <TarInput
-      class="col"
-      disabled
-      floating
-      id="speed-base"
-      :label="t('pokemon.statistic.select.options.Speed')"
-      :model-value="statistics.speed"
+      :id="`${formatForId(statistic)}-base`"
+      :label="t(`pokemon.statistic.select.options.${formatForLabel(statistic)}`)"
+      :model-value="statistics[statistic]"
       step="1"
       type="number"
     />
