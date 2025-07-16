a42817761e7a2a29197959575f3e3416e7f87ee6
Francis Pion <francispion@hotmail.com>
2025-07-16 15:44:00 -0400

    fix

--- backend/src/PokeGame.Core/Species/SpeciesId.cs
+++ backend/src/PokeGame.Core/Species/SpeciesId.cs
@@ -1,42 +1,33 @@
 ﻿using Krakenar.Core;
-using Krakenar.Core.Realms;
 using Logitar.EventSourcing;
 
 namespace PokeGame.Core.Species;
 
 public readonly struct SpeciesId
 {
-  private const string EntityType = "Content";
+  private const string EntityType = "Species";
 
   public StreamId StreamId { get; }
   public string Value => StreamId.Value;
 
-  public RealmId? RealmId { get; }
-  public Guid EntityId { get; }
-
-  public SpeciesId(Guid entityId, RealmId? realmId = null)
-  {
-    StreamId = IdHelper.Construct(EntityType, entityId, realmId);
-
-    EntityId = entityId;
-    RealmId = realmId;
-  }
   public SpeciesId(StreamId streamId)
   {
     StreamId = streamId;
-
-    Tuple<Guid, RealmId?> values = IdHelper.Deconstruct(streamId, EntityType);
-    EntityId = values.Item1;
-    RealmId = values.Item2;
   }
-  public SpeciesId(string value) : this(new StreamId(value))
+  public SpeciesId(Guid value)
   {
+    StreamId = IdHelper.Construct(EntityType, value);
   }
+  public SpeciesId(string value)
+  {
+    StreamId = new StreamId(value);
+  }
+
+  public static SpeciesId NewId() => new(Guid.NewGuid());
+  public Guid ToGuid() => IdHelper.Deconstruct(StreamId, EntityType).Item1;
 
   public static bool operator ==(SpeciesId left, SpeciesId right) => left.Equals(right);
   public static bool operator !=(SpeciesId left, SpeciesId right) => !left.Equals(right);
-
-  public static SpeciesId NewId(RealmId? realmId = null) => new(Guid.NewGuid(), realmId);
 
   public override bool Equals([NotNullWhen(true)] object? obj) => obj is SpeciesId id && id.Value == Value;
   public override int GetHashCode() => Value.GetHashCode();
