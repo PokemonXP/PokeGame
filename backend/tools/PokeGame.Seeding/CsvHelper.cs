356dd7d107d8ae2c9adddfe9acc42bb4045767c3
Francis Pion <francispion@hotmail.com>
2025-07-16 20:37:00 -0400

    seed

--- backend/tools/PokeGame.Seeding/CsvHelper.cs
+++ backend/tools/PokeGame.Seeding/CsvHelper.cs
@@ -22,7 +22,7 @@ internal static class CsvHelper
     csv.Context.RegisterClassMap<MedicinePayload.Map>();
     csv.Context.RegisterClassMap<MovePayload.Map>();
     csv.Context.RegisterClassMap<PokeBallPayload.Map>();
-    csv.Context.RegisterClassMap<RegionPayload.Map>();
+    csv.Context.RegisterClassMap<SeedRegionPayload.Map>();
     csv.Context.RegisterClassMap<SpeciesPayload.Map>();
     csv.Context.RegisterClassMap<TechnicalMachinePayload.Map>();
     csv.Context.RegisterClassMap<TrainerPayload.Map>();
