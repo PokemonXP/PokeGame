985dcf688f9048e8eb4705aa899c109faa5eb356
Francis Pion <francispion@hotmail.com>
2025-07-26 19:52:22 -0400

    infrastructure

--- backend/src/PokeGame.Infrastructure/EventSerializer.cs
+++ backend/src/PokeGame.Infrastructure/EventSerializer.cs
@@ -18,6 +18,7 @@ internal class EventSerializer : Krakenar.Infrastructure.EventSerializer
     SerializerOptions.Converters.Add(new BoxConverter());
     SerializerOptions.Converters.Add(new CatchRateConverter());
     SerializerOptions.Converters.Add(new EggCyclesConverter());
+    SerializerOptions.Converters.Add(new EvolutionIdConverter());
     SerializerOptions.Converters.Add(new FormIdConverter());
     SerializerOptions.Converters.Add(new FriendshipConverter());
     SerializerOptions.Converters.Add(new GenderRatioConverter());
