0d13c91c9e9883b859ac22eafc002cbbe70ccb89
Francis Pion <francispion@hotmail.com>
2025-07-31 10:12:55 -0400

    POKEGAME-248: refactored block components.

--- frontend/src/components/pokemon/forms/PokemonFormBlock.vue
+++ frontend/src/components/pokemon/forms/PokemonFormBlock.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
 import { TarAvatar } from "logitar-vue3-ui";
 import { computed } from "vue";
+import { parsingUtils } from "logitar-js";
 
 import EditIcon from "@/components/icons/EditIcon.vue";
+import ExternalIcon from "@/components/icons/ExternalIcon.vue";
 import type { Form } from "@/types/pokemon-forms";
 
-const props = defineProps<{
-  form: Form;
-}>();
+const { parseBoolean } = parsingUtils;
+
+const props = withDefaults(
+  defineProps<{
+    external?: boolean | string;
+    form: Form;
+    size?: number | string;
+  }>(),
+  {
+    size: 40,
+  },
+);
 
 const displayName = computed<string>(() => props.form.displayName ?? props.form.uniqueName);
+const isExternal = computed<boolean>(() => parseBoolean(props.external) ?? false);
+const target = computed<string | undefined>(() => (isExternal.value ? "_blank" : undefined));
 </script>
 
 <template>
@@ -17,19 +30,27 @@ const displayName = computed<string>(() => props.form.displayName ?? props.form.
     <div class="d-flex">
       <div class="d-flex">
         <div class="align-content-center flex-wrap mx-1">
-          <RouterLink :to="{ name: 'FormEdit', params: { id: form.id } }">
-            <TarAvatar :display-name="displayName" icon="fas fa-masks-theater" :url="form.sprites.default" />
-          </RouterLink>
+          <slot name="avatar">
+            <RouterLink :to="{ name: 'FormEdit', params: { id: form.id } }" :target="target">
+              <TarAvatar :display-name="displayName" icon="fas fa-masks-theater" :size="size" :url="form.sprites.default" />
+            </RouterLink>
+          </slot>
         </div>
       </div>
       <div>
-        <RouterLink :to="{ name: 'FormEdit', params: { id: form.id } }">
-          <EditIcon /> {{ displayName }}
-          <template v-if="form.displayName">
-            <br />
-            {{ form.uniqueName }}
-          </template>
-        </RouterLink>
+        <slot name="before"></slot>
+        <slot name="contents">
+          <RouterLink :to="{ name: 'FormEdit', params: { id: form.id } }" :target="target">
+            <ExternalIcon v-if="isExternal" />
+            <EditIcon v-else />
+            {{ displayName }}
+            <template v-if="form.displayName">
+              <br />
+              {{ form.uniqueName }}
+            </template>
+          </RouterLink>
+        </slot>
+        <slot name="after"></slot>
       </div>
     </div>
   </div>
